c6f986faba08fa881c4a58f73ae92536b7a8efa1
madams23 <mark.adams@ed.ac.uk>
2019-11-12 16:13:48 +0000

    qsub scripts as separate files

---
+++ GSrmUKBB.frq.sh
@@ -0,0 +1,21 @@
+#$ -N GSrmUKBB.frq
+#$ -l h_rt=1:00:00
+#$ -l h_vmem=4G
+#$ -t 1-22
+#$ -cwd
+#$ -e logs
+#$ -o logs
+
+. /etc/profile.d/modules.sh
+module load igmm/apps/plink/1.90b4
+
+CHR=$SGE_TASK_ID
+
+plink \
+  --bfile GS20K_HRC_0.8_GCTA_${CHR} \
+  --pheno QCdGS20K_Pheno_dep_status_Unaff_b_only0_Aff_b_only1.txt \
+  --keep QCdGS20K_unrelated_t0.025.fam \
+  --remove SMR_Italian_Outliers_UKBoverlap.txt \
+  --freq case-control \
+  --out GenScot_MDD_rmUKBB.HRC.${CHR} \
+  --memory 3000
